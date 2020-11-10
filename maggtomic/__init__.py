ebeda0550bedb4f23f4cfb75644ab8fd3822dfd7
Donny Winston <dwinston@alum.mit.edu>
2020-11-10 10:38:37 -0500

    feat: mongo validation using JSON Schema

--- maggtomic/__init__.py
+++ maggtomic/__init__.py
@@ -1,7 +1,13 @@
 import os
 
 from dotenv import load_dotenv
-from pymongo import MongoClient, ASCENDING as ASC, DESCENDING as DESC, IndexModel
+from pymongo import (
+    MongoClient,
+    ASCENDING as ASC,
+    DESCENDING as DESC,
+    IndexModel,
+    WriteConcern,
+)
 
 load_dotenv()
 MONGO_CONNECTION_URI = os.getenv("MONGO_CONNECTION_URI")
@@ -31,10 +37,37 @@ INDEX_MODELS = [
 ]
 
 
-def create_collection(name, drop=True):
-    if drop:
+def create_collection(name, drop_guard=True):
+    if drop_guard and name in db.list_collection_names():
+        raise ValueError(f"collection `{name}` already exists in db.")
+    else:
         db.drop_collection(name)
-    collection = db[name]
+    collection = db.create_collection(
+        name,
+        write_concern=WriteConcern(w=1, j=True),
+        # TODO schema switch s.t. if attribute not in {objectIdFor(a) for a in {:value,:id,:uriref}},
+        #   value must be {bsonType: objectId},
+        #   else value has no bsonType restriction.
+        #   Possible with JSON Schema?
+        validator={
+            "$jsonSchema": {
+                "bsonType": "object",
+                "required": ["e", "a", "v", "t", "o"],
+                "properties": {
+                    "e": {"bsonType": "objectId", "title": "entity"},
+                    "a": {"bsonType": "objectId", "title": "attribute"},
+                    "v": {"title": "value"},
+                    "t": {"bsonType": "objectId", "title": "transaction"},
+                    "o": {
+                        "bsonType": "bool",
+                        "title": "operation",
+                        "description": "assertion (true) or retraction (false)",
+                    },
+                },
+                "additionalProperties": False,
+            }
+        },
+    )
     collection.create_indexes(INDEX_MODELS)
     return collection
 
@@ -49,7 +82,8 @@ def create_collection(name, drop=True):
 #     qudt:dataType (qudt:basis, qudt:cardinality, qudt:orderedType, qudt:pythonName, etc.), etc.
 #     to associate with the qudt:value Literal of a structured value.
 #  - "updating" and "deleting" needs to transact retraction statements.
+#  - use crockford base32 for user-shareable entity IDs stored as 64-bit integers.
 
 
 if __name__ == "__main__":
-    create_collection("main")
+    maincoll = create_collection("main", drop_guard=False)
