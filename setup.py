77a398845ac05d7d7bc5d8dae9535f99315881ee
Donny Winston <dwinston@alum.mit.edu>
2020-11-11 15:26:28 -0500

    feat(pypi): more info

--- setup.py
+++ setup.py
@@ -1,9 +1,22 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="maggtomic",
     url="https://github.com/polyneme/maggtomic",
     packages=find_packages(),
     use_scm_version=True,
     setup_requires=["setuptools_scm"],
+    author="Donny Winston",
+    author_email="donny@polyneme.xyz",
+    description="Metadata aggregation using the Datomic information model",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+    ],
+    python_requires=">=3.6",
 )
