a1d41cc84a703ac4c60b6f93dace5cdd74c4452a
Donny Winston <dwinston@alum.mit.edu>
2020-11-11 15:16:16 -0500

    feat: first try for pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='maggtomic',
+    name="maggtomic",
     url="https://github.com/polyneme/maggtomic",
     packages=find_packages(),
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
 )
